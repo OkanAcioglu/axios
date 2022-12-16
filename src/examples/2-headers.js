4c22b2b0f1154c75a1913c73d03f5164310535ae
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-16 14:50:24 +0300

    setup-headers

--- src/examples/2-headers.js
+++ src/examples/2-headers.js
@@ -1,15 +1,35 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from 'react'
+import axios from 'axios'
 
-const url = 'https://icanhazdadjoke.com/';
+//! When we need to provide more options such as "request headers" and for that we need pass additional argument and we will set it up as an object
+//! second argument --> axios.get(url,{})
+//! third argument --> axios.post(url,{data},{})
+
+const url = 'https://icanhazdadjoke.com/'
 // Accept : 'application/json'
 
 const Headers = () => {
-  const [joke, setJoke] = useState('random dad joke');
+  const [joke, setJoke] = useState('random dad joke')
 
   const fetchDadJoke = async () => {
-    console.log('fetch dad joke');
-  };
+    try {
+      // const resp = await axios(url)
+      // console.log(resp) //! we are not getting our data in json format so we will add headers...
+      //? First Approach
+      // const resp = await axios(url, {
+      //   headers: { Accept: 'application/json' },
+      // })
+      // console.log(resp)
+      //? Second Approach
+      const { data } = await axios(url, {
+        headers: { Accept: 'application/json' },
+      })
+      //console.log(data.joke)
+      setJoke(data.joke)
+    } catch (err) {
+      console.log(err.response)
+    }
+  }
 
   return (
     <section className='section text-center'>
@@ -18,6 +38,6 @@ const Headers = () => {
       </button>
       <p className='dad-joke'>{joke}</p>
     </section>
-  );
-};
-export default Headers;
+  )
+}
+export default Headers
