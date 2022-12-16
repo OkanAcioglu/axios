aec1cf4f4f8237636757d531caea65c9f728dcd3
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-16 11:06:37 +0300

    get-request

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,12 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
+//! import axios
+import axios from 'axios'
 // limit, if 429 wait for 15 min and try again
-const url = 'https://course-api.com/react-store-products';
+const url = 'https://course-api.com/react-store-productss'
 
 const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      //! by default axios use "get" method
+      const response = await axios(url)
+      console.log(response)
+      //! data is in the "data" property
+      const data = response.data
+      console.log(data)
+      //! Unlike the fetch (fetch only treats the network errors as errors), axios treats all of the errors
+    } catch (error) {
+      console.log(error.response) //! Whole response
+      console.log(error.response.data) //! only error data
+    }
+  }
   useEffect(() => {
-    console.log('first axios request');
-  }, []);
+    fetchData()
+    console.log('first axios request')
+  }, [])
 
-  return <h2 className='text-center'>first request</h2>;
-};
-export default FirstRequest;
+  return <h2 className='text-center'>first request</h2>
+}
+export default FirstRequest
